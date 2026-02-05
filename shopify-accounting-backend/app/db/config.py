473dea1640661a137ed60e06b33f2e9c95a5d4e5
zen-patilrohit <patil.rohit14@gmail.com>
2026-02-05 18:27:57 +0530

    config changes for shopify

--- shopify-accounting-backend/app/db/config.py
+++ shopify-accounting-backend/app/db/config.py
@@ -1,6 +1,18 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 ENV = os.getenv("ENV", "production")
 USE_OAUTH  = ENV == "production"
 SHOPIFY_API_KEY = os.getenv("SHOPIFY_API_KEY")
 SHOPIFY_API_SECRET = os.getenv("SHOPIFY_API_SECRET")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT")
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
